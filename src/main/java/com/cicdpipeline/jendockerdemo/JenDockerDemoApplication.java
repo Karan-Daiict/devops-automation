d9995f5754e63df3a552ac60aade657c8ce7acdf
Karan Ladla <karan@gmail.com>
2022-08-30 11:27:19 +0530

    Docker file addition

--- src/main/java/com/cicdpipeline/jendockerdemo/JenDockerDemoApplication.java
+++ src/main/java/com/cicdpipeline/jendockerdemo/JenDockerDemoApplication.java
@@ -4,16 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootApplication
 public class JenDockerDemoApplication {
 
-	@GetMapping
-	public String message(){
-		return "Jenkins and Docker demo";
-	}
+    @GetMapping
+    public String message() {
+        return "Jenkins and Docker demo";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenDockerDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenDockerDemoApplication.class, args);
+    }
 
 }
